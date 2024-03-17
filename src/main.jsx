b8cd94ab4fb941417f69cd4145bcecbb8d608119
annysen <annysen95@gmail.com>
2024-03-17 13:23:50 +0600

    nav bar, and other componnet created. also tshirt.json file is called using useLoader

---
+++ src/main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import About from "./Components/About/About.jsx";
+import Main from "./Components/Layout/Main.jsx";
+import Home from "./Components/Home/Home.jsx";
+import OrderReview from "./Components/OrderReview.jsx/OrderReview.jsx";
+import Contact from "./Components/Contact/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: () => fetch("tshirts.json"),
+      },
+      {
+        path: "review",
+        element: <OrderReview></OrderReview>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
