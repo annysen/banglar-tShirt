9860f4fddf0c2f0bcb01e69915d2a0e37c7cb5d0
annysen <annysen95@gmail.com>
2024-03-18 23:38:51 +0600

    here conditional rendering e added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -15,12 +15,19 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
 
       <h4>{massage}</h4>
 
+      {cart.length > 3 ? (
+        <p>Congrats! you have added more then 3</p>
+      ) : (
+        <p>Add more product then 3</p>
+      )}
       {cart.map((ts) => (
         <p key={ts._id}>
           {ts.name}
           <button onClick={() => handleRemoveFromCart(ts._id)}>x</button>
         </p>
       ))}
+
+      {cart.length === 2 && <h3>You get Double Bonus</h3>}
     </div>
   );
 };
