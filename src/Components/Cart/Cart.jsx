a201443fc7a3260c8bb07e68c0397e9da82d4982
annysen <annysen95@gmail.com>
2024-03-18 23:31:02 +0600

    here conditional rendering with if/else added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,8 +1,19 @@
 const Cart = ({ cart, handleRemoveFromCart }) => {
   console.log(cart);
+
+  let massage;
+
+  if (cart.length) {
+    massage = "Here is your Product";
+  } else {
+    massage = "Hei add product";
+  }
+
   return (
     <div>
       <h4>Order Summary: {cart.length}</h4>
+
+      <h4>{massage}</h4>
 
       {cart.map((ts) => (
         <p key={ts._id}>
